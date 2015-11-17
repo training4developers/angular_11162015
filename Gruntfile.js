21de49f007f057c6b7caeb2d103f4c0417525dd7
Eric W. Greene <eric@ericwgreene.com>
2015-11-16 21:29:12 -0500

    added day 1 code demos

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		webServer: {
+			port: 8080,
+			folder: "app/www"
+		}
+	});
+
+	grunt.registerTask("default", function() {
+
+		var
+			http = require("http"),
+			express = require("express"),
+			app = express(),
+			server = http.createServer(app),
+			config = grunt.config();
+
+		app.use(express.static(config.webServer.folder));
+
+		server.listen(config.webServer.port, function() {
+			console.log("web server started on port " +
+				config.webServer.port);
+		});
+
+		this.async();
+
+	});
+
+};
